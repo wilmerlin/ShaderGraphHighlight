2a3d4db8430cab11fdc40bebf3c2fc9c574d9be5
Wilmer Lin <wilmerlin@gmail.com>
2019-05-02 17:02:08 -0700

    Tangram Puzzler updated with HighlightShaderGraph and Highlighter script

---
+++ Assets/RW/Scripts/Highlighter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class Highlighter : MonoBehaviour
+{
+    // reference to this MeshRenderer component
+    private MeshRenderer meshRenderer;
+    [SerializeField]
+
+    // non-highlighted material
+    private Material originalMaterial;
+
+    // glowing material
+    [SerializeField]
+    private Material highlightedMaterial;
+
+    void Start()
+    {
+        // cache a reference to the MeshRenderer
+        meshRenderer = GetComponent<MeshRenderer>();
+
+        // use non-highlighted material by default
+        EnableHighlight(false);
+    }
+
+    // toggle betweeen the original and highlighted materials
+    public void EnableHighlight(bool onOff)
+    {
+        if (meshRenderer != null && originalMaterial != null && highlightedMaterial != null)
+        {
+            meshRenderer.material = onOff ? highlightedMaterial : originalMaterial;
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        EnableHighlight(true);
+    }
+
+    private void OnMouseExit()
+    {
+        EnableHighlight(false);
+    }
+
+}
